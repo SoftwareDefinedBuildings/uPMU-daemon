d380f79722e098ebe0b1c9de37503ae44e45ee75
Michael Andersen <m.andersen@cs.berkeley.edu>
2014-06-08 12:29:32 -0700

    Tweak rsync script to include a lock

--- sync_csv.sh
+++ sync_csv.sh
@@ -9,7 +9,18 @@ LPATH="/var/www/www/csv/"
 eval $(ssh-agent)
 ssh-add remote_identity.pem
 
+if [ -e rsync.lock ]
+then
+	echo "Skipping, lock exists"
+	exit 1
+fi
+echo "adding the lock"
+touch rsync.lock
+
 for i in grizzly_peak switch_a6 soda_a soda_b
 do
     $RSYNC $REMOTE:$RBPATH/$i $LPATH/$i
 done
+
+echo "removing the lock"
+rm rsync.lock
