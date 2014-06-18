5a49561e104a55fcced2f4e8e7bbae01016df117
Sam Kumar <samkumar99@gmail.com>
2014-06-17 23:42:17 -0700

    Added script to read database and convert backup files to CSV

--- utils.py
+++ utils.py
@@ -27,6 +27,28 @@ def time_to_str(lst):
     time_rep = time_rep.replace(' ', '_')
     return time_rep
     
+def lst_to_rows(parsed):
+    rows = []
+    for s in parsed:
+        basetime = time_to_nanos(datetime.datetime(*s.sync_data.times))
+        # it seems s.sync_data.sampleRate is the number of milliseconds between samples
+        timedelta = 1000000 * s.sync_data.sampleRate # nanoseconds between samples
+        i = 0
+        while i < 120:
+            row = []
+            row.append(basetime + int((i * timedelta) + 0.5))
+            row.append(s.sync_data.lockstate[i])
+            for start in ('L', 'C'):
+                for num in xrange(1, 4):
+                    attribute = getattr(s.sync_data, '{0}{1}MagAng'.format(start, num))
+                    row.append(attribute[i].angle)
+                    row.append(attribute[i].mag)
+            row.append(s.gps_stats.satellites)
+            row.append(s.gps_stats.hasFix)
+            i += 1
+            rows.append(row)
+    return rows
+    
 def binsearch(sorted_lst, item):
     """ Returns the index if ITEM in SORTED_LST if it is in the list; otherwise it returns
     an index closest to where it would be. """
