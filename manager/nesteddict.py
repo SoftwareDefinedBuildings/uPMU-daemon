81142198ea9452d62978698e1f5f04e3040e56f1
Sam Kumar <samkumar99@gmail.com>
2014-11-22 20:24:18 -0800

    removed unnecessary functions and fixed bug regarding copying dictionaries

--- manager/nesteddict.py
+++ manager/nesteddict.py
@@ -24,3 +24,36 @@ def makeflat(dictionary, separator):
             for childkey in dictionary[key]:
                 dictionary[key + separator + childkey] = dictionary[key][childkey]
             del dictionary[key]
+
+def mergenesteddicts(base, overrides):
+    """ Merges OVERRIDES into BASE, overriding properties where necessary. If
+    additional dictionaries are contained as values, they are recursively
+    merged. """
+    for key in overrides:
+        if key in base and isinstance(base[key], dict) and isinstance(overrides[key], dict):
+                mergenesteddicts(base[key], overrides[key])
+        else:
+            base[key] = overrides[key]
+
+def deepequals(dict1, dict2):
+    keys1 = dict1.keys()
+    keys2 = dict2.keys()
+    if keys1 == keys2:
+        for key in keys1:
+            if isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
+                if not deepequals(dict1[key], dict2[key]):
+                    return False
+            elif dict1[key] != dict2[key]:
+                return False
+        return True
+    else:
+        return False
+
+def deepcopy(dictionary):
+    newdict = {}
+    for key in dictionary:
+        if isinstance(dictionary[key], dict):
+            newdict[key] = deepcopy(dictionary[key])
+        else:
+            newdict[key] = dictionary[key]
+    return newdict
